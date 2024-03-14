e75a98293ca116996927e6afb4474f7b3da4b540
Ch1m3r4Chaos <163418755+Ch1m3r4Chaos@users.noreply.github.com>
2024-03-14 19:36:10 +0530

    Create runMe.sh

---
+++ runMe.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+echo "I am a string of bits, oh so fine,"
+echo "With ones and zeros, in a line."
+echo "Count my ones, and if they're odd,"
+echo "A parity error might just be afoot, oh my god!"
+echo ""
+echo "But fear not, dear friend, don't you fret,"
+echo "For there's a simple trick you can set."
+echo "Check if the count is even, not odd,"
+echo "And you'll find peace, like a peaceful pond."
+echo ""
+echo "So tell me, wise one, what am I?"
+echo "A puzzle to solve, as time goes by."
+echo ""
+echo "Also, in my world, 0 represents '.' and 1 represents '-' and break is space "
+echo ""
+# Array of hardcoded 16-bit binary numbers
+
+binary_numbers=(
+    "0000100010000001" # - 
+    "0000000000000001"
+    "0000000000000010"
+    "0000000000000100"
+    "0000000000001000"
+    "0000000000010000"
+    "0000000000100000"
+    "0000000001000000"
+    "0000000010000000"
+    "0000000100000000"
+)
+
+echo "0000100010000001"
+read -p "Press Enter for next binary number..."
+
+echo "0010100010000001"
+read -p "Press Enter for next binary number..."
+
+echo "break"
+read -p "Press Enter for next binary number..."
+
+echo "0000000000000000"
+read -p "Press Enter for next binary number..."
+
+echo "break"
+read -p "Press Enter for next binary number..."
+
+echo "0000100010000111"
+read -p "Press Enter for next binary number..."
+
+echo "0100000000000001"
+read -p "Press Enter for next binary number..."
+
+echo "0000000000000000"
+read -p "Press Enter for next binary number..."
+
+echo "0100100010001001"
+read -p "Press Enter for next binary number..."
+
+echo "break"
+read -p "Press Enter for next binary number..."
+
+echo "0000100010001001"
+read -p "Press Enter for next binary number..."
+
+echo "0000100010010001"
+read -p "Press Enter for next binary number..."
+
+echo "0000100010000001"
+read -p "Press Enter for next binary number..."
+
+echo "break"
+read -p "Press Enter for next binary number..."
+
+echo "0111100000000000"
+read -p "Press Enter for next binary number..."
+
+echo "0000000011111111"
+read -p "Press Enter for next binary number..."
+
+echo "0000000000000000"
+read -p "Press Enter for next binary number..."
+
+echo "Listen closely to the dots and dashes"
